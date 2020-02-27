aff3b3ed6ea09f182072a390f79659a1ad2b8dda
dannyLiu <h152074716@gmail.com>
2020-02-27 17:04:32 +0800

    定義要抓取的meta property

---
+++ ConsoleApp1/Meta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Meta
+    {
+        public string url { get; set; }
+        public string title {get;set;}
+        public string site_name { get; set; }
+        public string image { get; set; }
+        public string description { get; set; }
+        public string updated_time { get; set; }
+        public string published_time { get; set; }
+    }
+}
