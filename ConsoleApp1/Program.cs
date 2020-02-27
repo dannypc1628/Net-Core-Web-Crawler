e2562fa19985df449449274f1a9cb400ecfd4a2c
dannyLiu <h152074716@gmail.com>
2020-02-27 17:06:47 +0800

    Program.Main改用非同步呼叫爬蟲程式

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        static async Task Main(string[] args)
+        {            
+            string url ;
+            url = "https://dannyliu.me/%e5%9c%a8%e7%8f%be%e6%9c%89asp-net-mvc%e5%b0%88%e6%a1%88%e4%b8%ad%e5%8a%a0%e5%85%a5asp-net-identity/";
+                       
+            Crawler c = new Crawler();
+            await c.Get(url);
+
+            Console.WriteLine("End~~~");
+            Console.Read();
+        }        
     }
 }
