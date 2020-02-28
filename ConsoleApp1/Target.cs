5ac7dcabb4fa6a4d499d40b20e831918f0574509
dannyLiu <h152074716@gmail.com>
2020-02-28 14:47:38 +0800

    拿取遠端JSON資料

---
+++ ConsoleApp1/Target.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleApp1.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Target
+    {
+        HttpClient HttpClient { get; }
+        public Target()
+        {
+            var serviceProvider = new ServiceCollection().AddHttpClient().BuildServiceProvider();
+
+            var httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
+
+            HttpClient = httpClientFactory.CreateClient();
+        }
+        
+        public async Task<string> Get(string url)
+        {
+            var responseMessage = await HttpClient.GetAsync(url);
+            if (true == responseMessage.IsSuccessStatusCode)
+            {
+                var dataResult = await responseMessage.Content.ReadAsStringAsync();
+                //var list = JsonSerializer.Deserialize<List<object>>(dataResult);
+                
+                Console.WriteLine(dataResult);
+                return dataResult;
+            }
+            else{
+                if (responseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine("404 NotFound");
+                    return "";
+                }
+                else
+                {
+                    Console.WriteLine(responseMessage.StatusCode);
+                    return "";
+                }
+            }
+        }
+
+        
+    }
+}
