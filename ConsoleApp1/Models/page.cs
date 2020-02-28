0b86e03be71b12f23b165695a705b6b004d988db
dannyLiu <h152074716@gmail.com>
2020-02-28 14:50:00 +0800

    定義遠端資料的類別

---
+++ ConsoleApp1/Models/page.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    class page
+    {
+        public string title { get; set; }
+
+        public string url { get; set; }
+    }
+}
---
+++ ConsoleApp1/Models/w3hexschoolData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    class w3hexschoolData
+    {
+        public string name { get; set; }
+
+        public string blogUrl { get; set; }
+
+        public string updateTime { get; set; }
+
+        public IEnumerable<page> blogList { get;set;}
+    }    
+}
