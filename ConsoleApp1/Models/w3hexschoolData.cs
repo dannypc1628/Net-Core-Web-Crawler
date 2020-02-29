654cc0c0ca4a2a099d8d1551edfe71a954108d40
dannyLiu <h152074716@gmail.com>
2020-02-29 19:03:22 +0800

    w3hexschoolData的blogList的型別改為List

--- ConsoleApp1/Models/w3hexschoolData.cs
+++ ConsoleApp1/Models/w3hexschoolData.cs
@@ -12,6 +12,6 @@ namespace ConsoleApp1.Models
 
         public string updateTime { get; set; }
 
-        public IEnumerable<page> blogList { get;set;}
+        public List<page> blogList { get;set;}
     }    
 }
